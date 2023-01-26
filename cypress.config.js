887cc03c24d5527985c7312851eea5996367d910
Natalia Kazakova <npkazakova@gmail.com>
2023-01-26 16:45:47 +0700

    settings

--- cypress.config.js
+++ cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
   defaultCommandTimeout: 15000,
   requestTimeout: 15000,
   e2e: {
-    baseUrl: 'https://qatest.site',
+    baseUrl: 'https://tms1.12go.io',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
